56e611f86fc31baf79dc38b73a9c99a3bcf10a05
shemaikuzwe <eshemaikuzwe@gmail.com>
2025-03-15 12:20:50 +0200

    fix compose yaml

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserMiddleware } from './middleware/user.middleware';
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
-import { UserMiddleware } from './middleware/user.middleware';
 
 @Module({
   imports: [UserModule, AuthModule],
